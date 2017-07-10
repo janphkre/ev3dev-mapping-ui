10baf4a67109996085acb55a9752793f6ff69d5c
janphkre <janphkre@gmx.de>
2017-07-11 00:29:54 +0200

    Implemented Geometry-Methods.

--- Assets/Scripts/Society/Util/Geometry.cs
+++ Assets/Scripts/Society/Util/Geometry.cs
@@ -3,15 +3,17 @@ using UnityEngine;
 
 class Geometry {
 
-    //Calculates the minimum distance between two lines.
+    //Calculates the distance between two points.
     public static float Distance(Vector2 a, Vector2 b) {
         return Math.Abs((b - a).magnitude);
     }
 
-    /*public static Vector2 Center(Vector4 a) {
-        return new Vector2(a.x+0.5f*(a.z-a.x),a.y+0.5f*(a.w-a.y));
-    }*/
+    //Calculates the mahalanobis distance between two points.
+    public static float MahalanobisDistance(Vector2 a, Vector2 b) {
+        throw new NotImplementedException();
+    }
 
+    //Calculates the maximum distance from a pose to all features:
     public static float Radius(Vector2 pos, Vector2[] features) {
         float max = 0f;
         foreach(Vector4 feature in features) {
@@ -21,18 +23,30 @@ class Geometry {
         return max;
     }
 
-    //Feature is rotated around end by z (z should be degrees)
-    public static Vector2 Rotate(Vector2 feature, Vector2 end, float z) {
-        throw new NotImplementedException();
+    //Feature is rotated around end by z (in degrees)
+    public static Vector2 Rotate(Vector2 feature, Vector3 end, float z) {
+        var f = ToRangeBearing(feature, end);
+        return FromRangeBearing(f.x, f.y + z, end);
     }
 
     //Returns the range and bearing form the robot to the feature.
     public static Vector2 ToRangeBearing(Vector2 feat, Vector3 origin) {
-        throw new NotImplementedException();
+        var f = feat - (Vector2) origin;
+        var result = new Vector2(Math.Abs(f.magnitude), origin.z);
+        result.y += (float)(Math.Atan2(f.x, f.y) * 180.0d / Math.PI);
+        result.y %= 360;
+        return result;
     }
 
-    //Returns the position of the feature from the range and bearing. (Converting polar coordinates to carthesic coordinates)
+    //Returns the position of the feature from the range and bearing. (Converting polar coordinates(with degrees) to carthesic coordinates)
     public static Vector2 FromRangeBearing(float posRange, float posBearing) {
-        throw new NotImplementedException();
+        double d = posBearing * Math.PI / 180.0d;
+        return new Vector2((float) (posRange * Math.Cos(d)), (float) (posRange * Math.Sin(d)));
+    }
+
+    //Returns the position of the feature from the range and bearing and the origin pose.
+    public static Vector2 FromRangeBearing(float posRange, float posBearing, Vector3 origin) {
+        double d = (posBearing+origin.z) * Math.PI / 180.0d;
+        return new Vector2((float)(posRange * Math.Cos(d)) + origin.x, (float)(posRange * Math.Sin(d)) + origin.y);
     }
 }
